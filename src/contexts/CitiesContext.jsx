f0296edc317d2a625ba3234984d6442e2bee2fe0
Mariia-Olena <mariia.stus@gmail.com>
2024-01-26 10:04:56 +0200

    implemet lazy loading

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,5 +1,10 @@
-import {} from 'react';
-import { useReducer, createContext, useEffect, useContext } from 'react';
+import {
+  useReducer,
+  createContext,
+  useEffect,
+  useContext,
+  useCallback,
+} from 'react';
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -85,21 +90,25 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
-    if (NUmber(id) === currentCity.id) return;
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
 
-    dispatch({ type: 'loading' });
-    try {
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-      dispatch({ type: 'city/loaded', payload: data });
-    } catch {
-      dispatch({
-        type: 'rejected',
-        payload: 'The was an error loading city...',
-      });
-    }
-  }
+      dispatch({ type: 'loading' });
+
+      try {
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: 'city/loaded', payload: data });
+      } catch {
+        dispatch({
+          type: 'rejected',
+          payload: 'The was an error loading city...',
+        });
+      }
+    },
+    [currentCity.id]
+  );
 
   async function createCity(newCity) {
     dispatch({ type: 'loading' });
